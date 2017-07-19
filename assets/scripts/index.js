13ab2c498c94a50a2932990ebdb4c354726ee217
coreyfedde <coreysfedde@gmail.com>
2017-07-19 16:43:23 -0400

    Create basic style and js
    Added basic styling to the to-do list and some js to make sure template
    is working.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -6,6 +6,25 @@ const config = require('./config')
 $(() => {
   setAPIOrigin(location, config)
 })
+$(() => {
+  alert('yep')
+})
+
+// let testF = function (arg) {
+//   $(arg.target).text('ok')
+// }
+
+$('#toDoInput').bind("enterKey",function(e){
+  console.log('enter')
+});
+
+$('#addTextButton').on("click", function () {
+  if ($('#toDoInput').val() !== '') {
+    let toDoItem = $('#toDoInput').val()
+    $('#toDoList').append(`<li> ${toDoItem} </li>`)
+    $('#toDoInput').val('')
+  }
+})
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
