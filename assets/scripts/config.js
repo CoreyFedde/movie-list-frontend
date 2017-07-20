6dfc5dc46602d0718a622c587be3923ffa3753ec
coreyfedde <coreysfedde@gmail.com>
2017-07-20 16:15:38 -0400

    Create authentication AJAX commands
    Wrote AJAX commands for the authentication protocols like log-in,
    log-out, sign-up, and change password. Linked those requests to modal
    forms and buttons.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,7 +1,10 @@
 'use strict'
 
+// ADD MODULE AND BUTTONS FOR AJAX THEN GO BACK INTO BACKEND AND USE DB TO CHECK
+
 const config = {
   apiOrigins: {
+    development: 'http://localhost:4741',
     production: 'https://ga-wdi-boston.herokuapp.com'
   }
 }
--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,21 +2,24 @@
 
 const setAPIOrigin = require('../../lib/set-api-origin')
 const config = require('./config')
+// const getFormFields = require('../../lib/get-form-fields.js')
+const api = require('./ajax/api.js')
 
 $(() => {
   setAPIOrigin(location, config)
-})
-$(() => {
-  alert('yep')
+  $('#signUpForm').on('submit', api.onSignUp)
+  $('#signInForm').on('submit', api.onLogIn)
+  $('#changePasswordForm').on('submit', api.onChangePassword)
+  $('#signOutButton').on('click', api.onLogOut)
 })
 
 // let testF = function (arg) {
 //   $(arg.target).text('ok')
 // }
 
-$('#toDoInput').bind("enterKey",function(e){
+$('#toDoInput').bind("enterKey", function(e){
   console.log('enter')
-});
+})
 
 $('#addTextButton').on("click", function () {
   if ($('#toDoInput').val() !== '') {
