303047337cd9cf591099880fc6bdb29793877bae
coreyfedde <coreysfedde@gmail.com>
2017-07-25 10:29:01 -0400

    Change url
    Changed production url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -5,7 +5,7 @@
 const config = {
   apiOrigins: {
     development: 'http://localhost:4741',
-    production: 'https://ga-wdi-boston.herokuapp.com'
+    production: 'https://pure-sands-32480.herokuapp.com/'
   }
 }
 
